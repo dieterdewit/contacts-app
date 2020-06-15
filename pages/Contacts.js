01bf677711e3ba4c0691c098cb537de15f877c69
Dieter de WIt <dewi15146@uvg.edu.gt>
2020-06-15 00:31:34 -0600

    Add Contacts & Doc. Pages

---
+++ pages/Contacts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from "next/head";
+
+export default function Contacts() {
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Contacts Book | Contacts App</title>
+                <meta
+                    name="description"
+                    content="Contacts Book Management and Views for Contacts App"
+                />
+            </Head>
+        </React.Fragment>
+    );
+}
---
+++ pages/Documentation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from "next/head";
+
+export default function Documentation() {
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Documentation | Contacts App</title>
+                <meta
+                    name="description"
+                    content="Documentation for Contacts App"
+                />
+            </Head>
+        </React.Fragment>
+    );
+}
